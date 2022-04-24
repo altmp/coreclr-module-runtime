cfaa9c84195b9c7f7f65e1eced522e4002348068
Fabian Terhorst <fabian.terhorst@gmail.com>
2022-04-24 22:33:46 +0200

    shared extern

--- c-api/utils/export.h
+++ c-api/utils/export.h
@@ -3,10 +3,11 @@
 
 #ifdef ALTV_CSHARP_SHARED
     #define EXPORT_CLIENT EXPORT
+    #define EXPORT_SHARED EXPORT
 #else
     #define EXPORT_CLIENT extern "C"
+    #define EXPORT_SHARED extern "C"
 #endif
 
 #define EXPORT_SERVER EXPORT
-#define EXPORT_SHARED EXPORT
 #define NO_GC
