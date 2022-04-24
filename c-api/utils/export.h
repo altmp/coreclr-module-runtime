6cc6ddce6c7f01aa3a389cb3f5c38965137b8972
Fabian Terhorst <fabian.terhorst@gmail.com>
2022-04-24 22:26:32 +0200

    fix shared export

--- c-api/utils/export.h
+++ c-api/utils/export.h
@@ -2,9 +2,9 @@
 #include "../../cpp-sdk/SDK.h"
 
 #ifdef ALTV_CSHARP_SHARED
-    #define EXPORT_CLIENT extern "C"
-#else
     #define EXPORT_CLIENT EXPORT
+#else
+    #define EXPORT_CLIENT extern "C"
 #endif
 
 #define EXPORT_SERVER EXPORT
