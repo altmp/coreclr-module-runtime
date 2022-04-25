942e8e445a4a183305252dfcc63bbc8b5f34e384
Dzhemesiuk Artem <programming@zziger.me>
2022-04-25 13:19:25 +0300

    Export macro fix

--- c-api/utils/export.h
+++ c-api/utils/export.h
@@ -1,13 +1,7 @@
 #pragma once
 #include "../../cpp-sdk/SDK.h"
 
-#ifdef ALTV_CSHARP_SHARED
-    #define EXPORT_CLIENT EXPORT
-    #define EXPORT_SHARED EXPORT
-#else
-    #define EXPORT_CLIENT extern "C"
-    #define EXPORT_SHARED extern "C"
-#endif
-
+#define EXPORT_CLIENT EXPORT
+#define EXPORT_SHARED EXPORT
 #define EXPORT_SERVER EXPORT
 #define NO_GC
