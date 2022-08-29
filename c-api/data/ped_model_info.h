5a42d42e5f3956b5bab7ae227bc3762d612cf9a3
Artem Dzhemesiuk <programming@zziger.me>
2022-08-29 21:30:23 +0200

    Reworked ClrPedModelInfo struct

--- c-api/data/ped_model_info.h
+++ c-api/data/ped_model_info.h
@@ -2,29 +2,42 @@
 
 #include "cpp-sdk/ICore.h"
 
+struct ClrBoneInfo
+{
+    uint16_t id;
+    uint16_t index;
+    char name[256];
+    
+    ClrBoneInfo() = default;
+    ClrBoneInfo(alt::BoneInfo info) : id(info.id), index(info.index), name() {
+        std::copy(info.name.begin(), info.name.size() >= 255 ? info.name.begin() + 255 : info.name.end(), name);
+        name[std::min((int) info.name.size(), 255)] = '\0';
+    }
+};
+
 struct ClrPedModelInfo {
     char* name = nullptr;
-    uint32_t hash;
+    uint32_t hash = 0;
         
-    alt::BoneInfo* bones;
-    uint32_t boneSize;
+    ClrBoneInfo* bones = nullptr;
+    uint32_t boneSize = 0;
 
     ClrPedModelInfo() = default;
-
-    ClrPedModelInfo(alt::PedModelInfo info) :
-            hash(info.hash) {
+    ClrPedModelInfo(alt::PedModelInfo info) : hash(info.hash) {
         name = new char[info.name.length() + 1];
-        strcpy(name, info.name.c_str());
+        std::copy(info.name.begin(), info.name.end(), name);
+        name[info.name.length()] = '\0';
 
-        auto pedModelBones = info.bones;
+        const auto pedModelBones = info.bones;
         boneSize = pedModelBones.size();
-        bones = new alt::BoneInfo[boneSize];
-        for (auto i = 0; i < boneSize; i++) {
-            bones[i] = pedModelBones[i];
+        bones = new ClrBoneInfo[boneSize];
+        for (uint32_t i = 0; i < boneSize; i++) {
+            bones[i] = ClrBoneInfo(pedModelBones[i]);
         }
     }
 
     ~ClrPedModelInfo() {
         delete[] name;
+        delete[] bones;
     }
 };
