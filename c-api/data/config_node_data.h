3d7faed567a8158341a375840a79ee55ff61e940
doxoh <till@tillschreiber.de>
2022-10-14 16:02:01 +0200

    first changes for config change

--- c-api/data/config_node_data.h
+++ c-api/data/config_node_data.h
@@ -1,26 +1,41 @@
 #pragma once
 
 struct ClrConfigNodeData {
-    uint8_t type = (uint8_t) alt::config::Node::Type::NONE;
+    uint8_t type = (uint8_t) Config::Value::Type::NONE;
     char* strValue = nullptr;
+    uint8_t boolValue = 0;
+    int64_t numValue = 0;
     int32_t elements = 0;
     char** keys = nullptr;
     ClrConfigNodeData** values = nullptr;
 
-    ClrConfigNodeData(alt::config::Node node) {
-        auto nodeType = node.GetType();
+    ClrConfigNodeData(Config::Value::ValuePtr node) {
+        auto nodeType = node->GetType();
         type = (uint8_t) nodeType;
         switch (nodeType) {
-            case alt::config::Node::Type::SCALAR: {
-                const auto str = node.ToString();
+            case Config::Value::Type::STRING: {
+                const auto str = node->As<std::string>();
+
                 strValue = new char[str.length() + 1];
                 std::copy(str.begin(), str.end(), strValue);
                 strValue[str.length()] = '\0';
+
                 break;
             }
-            
-            case alt::config::Node::Type::LIST: {
-                const auto list = node.ToList();
+            case Config::Value::Type::BOOL: {
+                const auto nodeBool = node->As<bool>();
+
+                boolValue = nodeBool;
+                break;
+            }
+            case Config::Value::Type::NUMBER: {
+                const auto nodeNumber = node->As<double>();
+
+                numValue = nodeNumber;
+                break;
+            }
+            case Config::Value::Type::LIST: {
+                const auto list = node->As<std::vector<Config::Value::ValuePtr>>();
                 elements = list.size();
                 values = new ClrConfigNodeData*[elements];
                 for (int i = 0; i < elements; i++) {
@@ -29,8 +44,8 @@ struct ClrConfigNodeData {
                 break;
             }
             
-            case alt::config::Node::Type::DICT: {
-                const auto dict = node.ToDict();
+            case Config::Value::Type::DICT: {
+                const auto dict = node->As<std::map<std::string, Config::Value::ValuePtr>>();
                 elements = dict.size();
                 keys = new char*[elements];
                 values = new ClrConfigNodeData*[elements];
@@ -45,7 +60,7 @@ struct ClrConfigNodeData {
                 break;
             }
             
-            case alt::config::Node::Type::NONE:
+            case Config::Value::Type::NONE:
             default:
                 break;
         }
@@ -54,7 +69,7 @@ struct ClrConfigNodeData {
     ~ClrConfigNodeData() {
         delete[] strValue;
         for (int i = 0; i < elements; i++) {
-            if (type == (uint8_t) alt::config::Node::Type::DICT)
+            if (type == (uint8_t) Config::Value::Type::DICT)
                 delete[] keys[i];
             delete values[i];
         }
