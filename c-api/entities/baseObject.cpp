8528284ef15618c25a4961c3db442ca3262254ba
Artem Dzhemesiuk <zziger07@gmail.com>
2022-11-21 18:29:51 +0100

    Entity caching

--- c-api/entities/baseObject.cpp
+++ c-api/entities/baseObject.cpp
@@ -18,6 +18,22 @@ alt::MValueConst* BaseObject_GetMetaData(alt::IBaseObject* baseObject, const cha
     return new MValueConst(baseObject->GetMetaData(key));
 }
 
+void* BaseObject_TryCache(alt::IBaseObject* baseObject) {
+    switch (baseObject->GetType()) {
+        case alt::IBaseObject::Type::PLAYER:
+            return dynamic_cast<alt::IPlayer*>(new cache::CachedPlayer(dynamic_cast<alt::IPlayer*>(baseObject)));
+        case alt::IBaseObject::Type::VEHICLE: {
+            return dynamic_cast<alt::IVehicle*>(new cache::CachedVehicle(dynamic_cast<alt::IVehicle*>(baseObject)));
+        }
+        default:
+            return nullptr;
+    }
+}
+
+void BaseObject_DestructCache(alt::IBaseObject* baseObject) {
+    delete dynamic_cast<cache::CachedBaseObject*>(baseObject);
+}
+
 uint8_t BaseObject_GetType(alt::IBaseObject* baseObject)
 {
     return (uint8_t) baseObject->GetType();
--- c-api/entities/baseObject.h
+++ c-api/entities/baseObject.h
@@ -11,6 +11,9 @@
 #include "../data/cloth.h"
 #include "../data/head_overlay.h"
 #include "../utils/export.h"
+#include "c-api/cache/CachedBaseObject.h"
+#include "c-api/cache/CachedPlayer.h"
+#include "c-api/cache/CachedVehicle.h"
 
 #ifdef __clang__
 #pragma clang diagnostic pop
@@ -20,5 +23,7 @@ EXPORT_SHARED void BaseObject_SetMetaData(alt::IBaseObject* baseObject, const ch
 EXPORT_SHARED uint8_t BaseObject_HasMetaData(alt::IBaseObject* baseObject, const char* key);
 EXPORT_SHARED void BaseObject_DeleteMetaData(alt::IBaseObject* baseObject, const char* key);
 EXPORT_SHARED alt::MValueConst* BaseObject_GetMetaData(alt::IBaseObject* baseObject, const char* key);
+EXPORT_SHARED void* BaseObject_TryCache(alt::IBaseObject* baseObject);
+EXPORT_SHARED void BaseObject_DestructCache(alt::IBaseObject* baseObject);
 
 EXPORT_SHARED uint8_t BaseObject_GetType(alt::IBaseObject* baseObject);
