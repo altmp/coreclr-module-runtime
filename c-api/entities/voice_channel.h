58c0c0bcb7fc3388fe12f0d36eb3862daeb1b5ef
doxoh <till@tillschreiber.de>
2023-08-14 13:17:58 +0200

    ad VoiceChannel_GetPlayerCount

--- c-api/entities/voice_channel.h
+++ c-api/entities/voice_channel.h
@@ -17,19 +17,19 @@ EXPORT_SHARED uint32_t VoiceChannel_GetID(alt::IVoiceChannel* voiceChannel);
 
 EXPORT_SERVER alt::IBaseObject* VoiceChannel_GetBaseObject(alt::IVoiceChannel* voiceChannel);
 
-EXPORT_SERVER alt::MValueConst* VoiceChannel_GetMetaData(alt::IVoiceChannel* voiceChannel, const char* key);
-EXPORT_SERVER void VoiceChannel_SetMetaData(alt::IVoiceChannel* channel, const char* key, alt::MValueConst* val);
-EXPORT_SERVER uint8_t VoiceChannel_HasMetaData(alt::IVoiceChannel* voiceChannel, const char* key);
-EXPORT_SERVER void VoiceChannel_DeleteMetaData(alt::IVoiceChannel* voiceChannel, const char* key);
-
-EXPORT_SERVER void VoiceChannel_AddPlayer(alt::IVoiceChannel* channel, alt::IPlayer* player);
-EXPORT_SERVER void VoiceChannel_RemovePlayer(alt::IVoiceChannel* channel, alt::IPlayer* player);
-EXPORT_SERVER void VoiceChannel_MutePlayer(alt::IVoiceChannel* channel, alt::IPlayer* player);
-EXPORT_SERVER void VoiceChannel_UnmutePlayer(alt::IVoiceChannel* channel, alt::IPlayer* player);
-EXPORT_SERVER uint8_t VoiceChannel_HasPlayer(alt::IVoiceChannel* channel, alt::IPlayer* player);
-EXPORT_SERVER uint8_t VoiceChannel_IsPlayerMuted(alt::IVoiceChannel* channel, alt::IPlayer* player);
 EXPORT_SERVER uint8_t VoiceChannel_IsSpatial(alt::IVoiceChannel* channel);
 EXPORT_SERVER float VoiceChannel_GetMaxDistance(alt::IVoiceChannel* channel);
+
+EXPORT_SERVER uint8_t VoiceChannel_HasPlayer(alt::IVoiceChannel* channel, alt::IPlayer* player);
+EXPORT_SERVER void VoiceChannel_AddPlayer(alt::IVoiceChannel* channel, alt::IPlayer* player);
+EXPORT_SERVER void VoiceChannel_RemovePlayer(alt::IVoiceChannel* channel, alt::IPlayer* player);
+
+EXPORT_SERVER uint8_t VoiceChannel_IsPlayerMuted(alt::IVoiceChannel* channel, alt::IPlayer* player);
+EXPORT_SERVER void VoiceChannel_MutePlayer(alt::IVoiceChannel* channel, alt::IPlayer* player);
+EXPORT_SERVER void VoiceChannel_UnmutePlayer(alt::IVoiceChannel* channel, alt::IPlayer* player);
+
+EXPORT_SERVER alt::IPlayer** VoiceChannel_GetPlayers(alt::IVoiceChannel* channel, uint64_t& size);
+EXPORT_SERVER uint64_t VoiceChannel_GetPlayerCount(alt::IVoiceChannel* channel);
 
 EXPORT_SERVER uint32_t VoiceChannel_GetFilter(alt::IVoiceChannel* channel);
 EXPORT_SERVER void VoiceChannel_SetFilter(alt::IVoiceChannel* channel, uint32_t filter);
