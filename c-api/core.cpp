d2cef90cf55c5b69ee84b9a8e4fcc3fa669e3e9d
Vektor <whiteman.chr@hotmail.com>
2023-10-02 10:17:17 +0200

    update sdk

--- c-api/core.cpp
+++ c-api/core.cpp
@@ -1628,7 +1628,7 @@ alt::IWebSocketClient* Core_CreateWebsocketClient(alt::ICore* core, alt::IResour
 }
 
 alt::IAudio* Core_CreateAudio(alt::ICore* core, const char* source, float volume, uint8_t isRadio, const char* basePath, alt::IResource* resource, uint32_t &id) {
-    auto audio = core->CreateAudio(source, volume, isRadio, basePath, resource);
+    auto audio = core->CreateAudio(source, volume, isRadio, true, basePath, resource);
     if (audio != nullptr) {
         id = audio->GetID();
     }
