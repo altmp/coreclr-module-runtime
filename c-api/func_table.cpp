0fcb3f18f52a3552eafdae6db2b080b6eb53c8e2
doxoh <till@tillschreiber.de>
2023-10-21 16:23:50 +0200

    fix func

--- c-api/func_table.cpp
+++ c-api/func_table.cpp
@@ -1,10 +1,6 @@
 #include "func_table.h"
 
-<<<<<<< HEAD
-inline uint64_t capiHash = 4677548953854785821UL;
-=======
 inline uint64_t capiHash = 16677003916101455049UL;
->>>>>>> release
 inline uint64_t capiHashes[] = {
     0,
     #ifdef ALT_CLIENT_API
@@ -1062,11 +1058,8 @@ inline uint64_t capiHashes[] = {
     13737530370025977174UL,
     7482854450085275693UL,
     2139798095052897524UL,
-<<<<<<< HEAD
-=======
     2001253713694863162UL,
     3166577616070768315UL,
->>>>>>> release
     15652019729912249391UL,
     8454955647873390265UL,
     5411021830103603795UL,
@@ -2810,11 +2803,8 @@ inline void* capiPointers[] = {
     (void*) Core_GetEventTypeSize,
     (void*) Core_GetMarkers,
     (void*) Core_GetMetaData,
-<<<<<<< HEAD
-=======
     (void*) Core_GetMValueArgsSize,
     (void*) Core_GetMValueSize,
->>>>>>> release
     (void*) Core_GetNetTime,
     (void*) Core_GetNetworkObjects,
     (void*) Core_GetPeds,
